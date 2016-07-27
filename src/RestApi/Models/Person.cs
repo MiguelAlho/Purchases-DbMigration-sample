7177489d8f6aa799e6f82bbd9bbebab79d61499d
Miguel Alho <alho@miguelalho.com>
2016-07-27 11:10:02 +0100

    Added column split migration for first and last name. files have version number appended for switch during demo

--- src/RestApi/Models/Person.cs
+++ src/RestApi/Models/Person.cs
@@ -2,6 +2,8 @@
 
 namespace RestApi.Models
 {
+
+    /*
     public class Person
     {
         public Person(Guid id, String fullName)
@@ -15,5 +17,29 @@ namespace RestApi.Models
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
+    }
+
+    */
+
+
+    public class Person
+    {
+        public Person(Guid id, string firstname, string lastname)
+        {
+            if (string.IsNullOrWhiteSpace(firstname))
+                throw new ArgumentException(nameof(firstname));
+            if (string.IsNullOrWhiteSpace(lastname))
+                throw new ArgumentException(nameof(lastname));
+
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get { return $"{FirstName} {LastName}"; } }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
     }
 }
